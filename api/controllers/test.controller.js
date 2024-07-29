f5c42a2e879521e368b8a511077c097067581b4e
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-07-29 20:27:48 +0300

    added Token verification middleware

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,32 +1,21 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const shouldBeLoggedIn = (req , res)=>{
-    const token = req.cookies.token
+export const shouldBeLoggedIn = async (req, res) => {
+  console.log(req.userId)
+  res.status(200).json({ message: "You are Authenticated" });
+};
 
-    if(!token){
-        return res.status(401).json({message:"Not Authenticated"});
+export const shouldBeAdmin = async (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is not Valid!" });
+    if (!payload.isAdmin) {
+      return res.status(403).json({ message: "Not authorized!" });
     }
+  });
 
-    jwt.verify(token,process.env.JWT_SECRET_KEY ,async (err, payload)=>{
-        if(err) return res.status(403).json({message:"Token is invalid"});
-
-    })
-
-    res.status(200).json({message:"Token is valid you are authenticated"});
-}
-export const shouldBeAdmin = (req , res)=>{
-    const token = req.cookies.token
-
-    if(!token){
-        return res.status(401).json({message:"Not Authenticated"});
-    }
-
-    jwt.verify(token,process.env.JWT_SECRET_KEY ,async (err, payload)=>{
-        if(err) return res.status(403).json({message:"Token is invalid"});
-
-        if(!payload.isAdmin) return res.status(403).json({message:"You are not Authorized"});
-
-    })
-
-    res.status(200).json({message:"Token is valid you are authenticated"});
-}
+  res.status(200).json({ message: "You are Authenticated" });
+};
